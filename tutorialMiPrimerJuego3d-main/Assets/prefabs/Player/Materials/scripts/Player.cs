ab23ae42cb886a6ed4bf4612bc2df9eb3d309ecc
Andycap98 <140664285+Andycap98@users.noreply.github.com>
2025-10-22 00:01:27 -0300

    mas cambios

--- tutorialMiPrimerJuego3d-main/Assets/prefabs/Player/Materials/scripts/Player.cs
+++
@@ -1,6 +0,0 @@
-public class Player
-{
-    public float velocity = 5f;      // Velocidad normal
-    public float acceleration = 10f; // Para movimiento acelerado
-    public float maxSpeed = 15f;     // Velocidad máxima
-}
